66bf892693f52f3d6806ca37960293ac2712ec2d
Vitaliy Buchincik <v.buchinchik@gmail.com>
2021-05-18 14:10:58 +0300

    Desktop version. Working on adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const nav = document.querySelector('.header__nav');
+const icon = document.querySelector('.header__right-part-menu');
+const menu = document.querySelector('#menu');
+
+menu.addEventListener('click', () => {
+  nav.classList.toggle('header__nav--active');
+});
+
+icon.addEventListener('click', () => {
+  icon.classList.toggle('header__right-part-menu--active');
+});
